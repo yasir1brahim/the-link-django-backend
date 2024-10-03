6ecee41d1be65e613f17dbf319514326ecdb6cbb
Avery Pawelek <averycpawelek@gmail.com>
2024-10-02 21:19:54 -0500

    Adding some values to serializers

--- apps/teams/signals.py
+++ apps/teams/signals.py
@@ -9,23 +9,24 @@ from .models import Invitation, Membership, Team
 from .invitations import process_invitation, get_invitation_id_from_request
 
 
-@receiver(user_signed_up)
-def add_user_to_team(request, user, **kwargs):
-    """
-    Adds the user to the team if there is invitation information in the URL.
-    """
-    invitation_id = get_invitation_id_from_request(request)
-    if invitation_id:
-        try:
-            invitation = Invitation.objects.get(id=invitation_id)
-            process_invitation(invitation, user)
-        except Invitation.DoesNotExist:
-            # for now just swallow missing invitation errors
-            # these should get picked up by the form validation
-            pass
-    elif not user.teams.exists():
-        # if the sign up was from a social account, there may not be a default team, so create one
-        create_default_team_for_user(user)
+# @receiver(user_signed_up)
+# def add_user_to_team(request, user, **kwargs):
+#     """
+#     Adds the user to the team if there is invitation information in the URL.
+#     """
+#     invitation_id = get_invitation_id_from_request(request)
+#     if invitation_id:
+#         try:
+#             invitation = Invitation.objects.get(id=invitation_id)
+#             process_invitation(invitation, user)
+#         except Invitation.DoesNotExist:
+#             # for now just swallow missing invitation errors
+#             # these should get picked up by the form validation
+#             pass
+#     elif not user.teams.exists():
+#         # if the sign up was from a social account, there may not be a default team, so create one
+#         # create_default_team_for_user(user)
+#         pass
 
 
 @receiver(post_save, sender=Membership)
