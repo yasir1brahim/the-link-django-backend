c8e697f9559d02cae98b1c173a94230fd6064ae9
Avery Pawelek <averycpawelek@gmail.com>
2024-10-24 11:36:45 -0500

    WIP on password reset, have email sending but need to set URL to frontend

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -1,6 +1,6 @@
 from dj_rest_auth.jwt_auth import get_refresh_view
 from dj_rest_auth.registration.views import RegisterView
-from dj_rest_auth.views import LogoutView, UserDetailsView, PasswordChangeView
+from dj_rest_auth.views import LogoutView, UserDetailsView, PasswordChangeView, PasswordResetView, PasswordResetConfirmView
 from django.urls import path
 from rest_framework_simplejwt.views import TokenVerifyView
 
