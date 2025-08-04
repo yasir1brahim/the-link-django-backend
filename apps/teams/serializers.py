c3599070e4ac6d45246aca29ade29b2151056c0e
Yasir Ibrahim <yasir.ibrahim@gmail.com>
2025-08-04 18:40:32 +0500

    Revert "TBL-446: Added the filter in the get_project method that only returns the filtered records for the project, if the user is either the member admin or the superuser for the project"
    This reverts commit 53d28c4ce2100e0c80cce2f33cc37bdaaf54d501.

--- apps/teams/serializers.py
+++ apps/teams/serializers.py
@@ -90,20 +90,7 @@ class TeamSerializer(WritableNestedModelSerializer, serializers.ModelSerializer)
         return super().create(validated_data)
     
     def get_projects(self, obj):
-        request = self.context.get('request')
-        if not request or not request.user.is_authenticated:
-            return []
-        
-        user = request.user
-        
-        if user.is_superuser:
-            projects = obj.project_set.all()
-        elif user.is_admin_for_team(obj):
-            projects = obj.project_set.all()
-        else:
-            projects = obj.project_set.filter(members=user)
-        
-        return BaseProjectSerializer(projects, many=True).data
+        return BaseProjectSerializer(obj.project_set.all(), many=True).data
     
     def get_active_flags(self, obj):
         return get_active_flags_for_team(obj)
