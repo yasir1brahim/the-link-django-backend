0addd3a7af966ff0922ca01d190fdc3a30cde8eb
Avery Pawelek <averycpawelek@gmail.com>
2025-10-16 10:54:23 -0500

    Fixing excel export issue

--- apps/deliverables/views/main_views.py
+++ apps/deliverables/views/main_views.py
@@ -19,6 +19,58 @@ from openpyxl.cell.cell import ILLEGAL_CHARACTERS_RE
 from openpyxl.utils import get_column_letter
 from openpyxl.styles import Alignment, Font, PatternFill, Border, Side
 
+
+def clean_excel_content(content):
+    """
+    Clean content for safe Excel/XML export by removing:
+    1. Illegal XML characters (control chars)
+    2. Zero-width characters
+    3. Special Unicode characters that cause XML parsing issues
+    4. All high Unicode characters that might not be XML-safe
+    """
+    if not content:
+        return ''
+    
+    # Convert to string first
+    content = str(content)
+    
+    # More aggressive approach: Keep ONLY XML 1.0 valid characters
+    # XML 1.0 valid characters are: #x9 | #xA | #xD | [#x20-#xD7FF] | [#xE000-#xFFFD] | [#x10000-#x10FFFF]
+    # But we'll be even more conservative and stick to common printable characters
+    def is_xml_safe(char):
+        code = ord(char)
+        # Tab, newline, carriage return
+        if code in (0x09, 0x0A, 0x0D):
+            return True
+        # Basic printable ASCII and common extended ASCII
+        if 0x20 <= code <= 0x7E:  # Standard ASCII printable
+            return True
+        # Common extended Latin characters (Western European)
+        if 0xA0 <= code <= 0xFF:
+            return True
+        # Allow some common Unicode ranges that are generally safe
+        # Latin Extended-A and Extended-B
+        if 0x0100 <= code <= 0x024F:
+            return True
+        # Greek and Coptic
+        if 0x0370 <= code <= 0x03FF:
+            return True
+        # Cyrillic
+        if 0x0400 <= code <= 0x04FF:
+            return True
+        # Everything else is potentially problematic - exclude it
+        return False
+    
+    # Filter to only safe characters
+    content = ''.join(char for char in content if is_xml_safe(char))
+    
+    # Truncate to safe length
+    max_length = 30000
+    if len(content) > max_length:
+        content = content[:max_length]
+    
+    return content
+
 from django.http import HttpResponse
 from django.conf import settings
 from django.shortcuts import get_object_or_404
@@ -750,7 +802,7 @@ class SubmittalItemViewSet(viewsets.ModelViewSet):
                     item.paragraph_number,
                     item.submittal_type,
                     item.submittal_description,
-                    re.sub(ILLEGAL_CHARACTERS_RE, '', item.submittal_content)
+                    clean_excel_content(item.submittal_content)
                 ]
                 worksheet.append(row)
                 for col in range(1, len(row) + 1):
@@ -772,7 +824,7 @@ class SubmittalItemViewSet(viewsets.ModelViewSet):
                     elif header_option['name'] == 'Submittal Title':
                         field_value = item.submittal_description
                     elif header_option['name'] == 'Submittal Description':
-                        field_value = re.sub(ILLEGAL_CHARACTERS_RE, '', item.submittal_content)
+                        field_value = clean_excel_content(item.submittal_content)
 
                     cell = worksheet.cell(row=row_idx, column=header_option['col'] + 1)
                     cell.value = field_value
